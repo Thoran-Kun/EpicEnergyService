d843bd54389f79959f0f89731c5b6c1c475daf93
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-02-23 12:34:40 +0100

    adds entities + enum (fatture)

---
+++ src/main/java/com/team6/EpicEnergyService/entities/Fattura.java
@@ -0,0 +1,36 @@
+package com.team6.EpicEnergyService.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "fatture")
+public class Fattura {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private LocalDate data;
+
+    private BigInteger importo;
+
+    private BigInteger numero;
+
+    @ManyToOne
+    @JoinColumn(name = "fatture_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "stato_fatture", nullable = false)
+    private StatoFattura stato;
+
+}
