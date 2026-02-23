3b02170e00945dc7d6ca4304b6d3184e8ce57c51
Salvatore <pepesalvatore12@gmail.com>
2026-02-23 11:42:30 +0100

    Initial commit

---
+++ src/main/java/com/team6/EpicEnergyService/EpicEnergyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team6.EpicEnergyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/EpicEnergyService/EpicEnergyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.EpicEnergyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
